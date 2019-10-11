b5d59cbecdc2963e0457241b3742202e9e4efdab
user <user@Ivans-MacBook-Pro-2.local>
2019-10-11 18:41:38 +0300

    Added field equality test

--- tutorial/quickstart/tests.py
+++ tutorial/quickstart/tests.py
@@ -6,12 +6,16 @@ class SerializerTestCase(TestCase):
         self.serializer_data = {
             'username':'test_user',
             'email':'test_email@mail.com',
-            'group':'test_group'
+            'groups':'test_group'
         }
-
-
         self.serializer = UserSerializer()
+
 
     def test_contains_expected_fields(self):
         data = self.serializer.data
         self.assertEqual(set(data.keys()), set(['username', 'email', 'groups']))
+
+    def test_color_must_be_in_choices(self):
+        serializer = UserSerializer(data=self.serializer_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.data, {'username' : 'test_user', 'email' : 'test_email@mail.com','groups' : 'test_group'})
