83f8155d22c709967a6c65b0eab14756f8af7e99
user <user@Ivans-MacBook-Pro-2.local>
2019-10-11 17:58:15 +0300

    Added simple test for heroku

--- tutorial/quickstart/tests.py
+++ tutorial/quickstart/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your tests here.
+class SerializerTestCase(TestCase):
+    def setUp(self):
+        self.serializer_data = {
+            'username':'test_user',
+            'email':'test_email@mail.com',
+            'group':'test_group'
+        }
+
+
+        self.serializer = UserSerializer()
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), set(['username', 'email', 'groups']))
